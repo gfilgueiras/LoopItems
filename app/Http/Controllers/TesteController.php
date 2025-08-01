89adf22d7868237ba5035aa92e2e96d211271e8b
Gustavo Filgueiras <gfilgueirasrj@gmail.com>
2025-08-01 11:25:49 -0300

    Copy

---
+++ app/Http/Controllers/TesteController.php
@@ -0,0 +1,27 @@
+
+/**
+ * Copyright (c) 2025 Sua Empresa. Todos os direitos reservados.
+ */
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class TesteController extends Controller
+{
+    public function gotta()
+    {
+        $users = User::all();
+
+        // Pode rtornar como JSON, vw ou qualquer outra resposta
+        return response()->json($users);
+    }
+
+    public function one()
+    {
+        return $this->gotta();
+    }
+}
