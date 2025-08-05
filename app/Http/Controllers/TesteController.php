da43c537773fe4a2c3ef9631182a623afc0bbc3c
Gustavo Filgueiras <gfilgueirasrj@gmail.com>
2025-08-05 00:36:42 -0300

    Copy-2

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -1,23 +1,23 @@
 <?php
 // ╔═════════════════════════════════════════════════════════════════════════════════════════════════════╗
-// ║ ◎                                                                                                 ◎ ║
-// ║     _____                                      _____                   _                            ║
-// ║    / ___ \       _                            (____ \                 | |                           ║
-// ║   | |   | | ____| |_  ___  ____  _   _  ___    _   \ \ ____ _   _ ____| | ___  ____   ____  ____    ║
-// ║   | |   | |/ ___)  _)/ _ \|  _ \| | | |/___)  | |   | / _  ) | | / _  ) |/ _ \|  _ \ / _  )/ ___)   ║
-// ║   | |___| ( (___| |_| |_| | | | | |_| |___ |  | |__/ ( (/ / \ V ( (/ /| | |_| | | | ( (/ /| |       ║
-// ║    \_____/ \____)\___)___/| ||_/ \____(___/   |_____/ \____) \_/ \____)_|\___/| ||_/ \____)_|       ║
-// ║                           |_|                                                 |_|                   ║
+// ║   ◎                                                                                               ║
+// ║       _____                                      _____                   _                          ║
+// ║      / ___ \       _                            (____ \                 | |                         ║
+// ║     | |   | | ____| |_  ___  ____  _   _  ___    _   \ \ ____ _   _ ____| | ___  ____   ____  ____  ║
+// ║     | |   | |/ ___)  _)/ _ \|  _ \| | | |/___)  | |   | / _  ) | | / _  ) |/ _ \|  _ \ / _  )/ ___) ║
+// ║     | |___| ( (___| |_| |_| | | | | |_| |___ |  | |__/ ( (/ / \ V ( (/ /| | |_| | | | ( (/ /| |     ║
+// ║      \_____/ \____)\___)___/| ||_/ \____(___/   |_____/ \____) \_/ \____)_|\___/| ||_/ \____)_|     ║
+// ║                             |_|                                                 |_|                 ║
 // ║                                                                                                     ║
-// ║   Last update:  05/08/2025 00:28:59                                                                 ║
-// ║   User update:  Gustavo Filgueiras <gfilgueirasrj@gmail.com>                                                                 ║
-// ║   Project:      Sou Nail Desing                                                                 ║
+// ║   Last update: 05/08/2025 00:36:42                                                                  ║
+// ║   User update: Gustavo Filgueiras <gfilgueirasrj@gmail.com>                                         ║
+// ║   Project: Sou Nail Desing                                                                          ║
 // ║                                                                                                     ║
-// ║   Author        Gustavo Filgueiras <gfilgueirasrj@gmail.com>                                                                       ║
-// ║   Created at:   05/08/2025 00:28:59                                                                 ║
-// ║   License       MIT                                                                    ║
-// ║   Copyright     2025 Octopus Developer                                                    ║
-// ║ ◎                                                                                                 ◎ ║
+// ║   Author Gustavo Filgueiras <gfilgueirasrj@gmail.com>                                               ║
+// ║   Created at: 05/08/2025 00:36:42                                                                   ║
+// ║   License MIT                                                                                       ║
+// ║   Copyright 2025 Octopus Developer                                                                  ║
+// ║   ◎                                                                                               ║
 // ╚═════════════════════════════════════════════════════════════════════════════════════════════════════╝
 
 declare(strict_types=1);
@@ -32,7 +32,7 @@ class TesteController extends Controller
     {
         $users = User::all();
 
-        // Pode rrar cmo JSON,vw qualquer our
+        // Pode rrar cmo JSON,vw qualquer ou
         return response()->json($users);
     }
 
