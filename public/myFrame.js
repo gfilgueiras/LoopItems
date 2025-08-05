2c5ca22a83db4f74890222c856e2931f37a22d42
Gustavo Filgueiras <gfilgueirasrj@gmail.com>
2025-08-05 01:13:24 -0300

    Copy-2

--- public/myFrame.js
+++ public/myFrame.js
@@ -7,12 +7,12 @@
 /* ║      \_____/ \____)\___)___/| ||_/ \____(___/   |_____/ \____) \_/ \____)_|\___/| ||_/ \____)_|     ║ */
 /* ║                             |_|                                                 |_|                 ║ */
 /* ║                                                                                                     ║ */
-/* ║   Last update: 05/08/2025 01:10:43                                                                  ║ */
+/* ║   Last update: 05/08/2025 01:13:24                                                                  ║ */
 /* ║   User update: Gustavo Filgueiras <gfilgueirasrj@gmail.com>                                         ║ */
 /* ║   Project:     Sou Nail Desing                                                                      ║ */
 /* ║                                                                                                     ║ */
 /* ║   Author:      Gustavo Filgueiras <gfilgueirasrj@gmail.com>                                         ║ */
-/* ║   Created at:  05/08/2025 01:05:43                                                                  ║ */
+/* ║   Created at:  05/08/2025 01:13:24                                                                  ║ */
 /* ║   License:     MIT                                                                                  ║ */
 /* ║   Copyright:   2025 Octopus Developer                                                               ║ */
 /* ╚═════════════════════════════════════════════════════════════════════════════════════════════════════╝ */
