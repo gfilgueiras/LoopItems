49cdff4f1951ceace4bdca5f01349d07ac89fae6
Gustavo Filgueiras <gfilgueirasrj@gmail.com>
2025-08-05 21:17:19 -0300

    Copy-2

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -8,7 +8,7 @@
 /* ║      \_____/ \____)\___)___/| ||_/ \____(___/   |_____/ \____) \_/ \____)_|\___/| ||_/ \____)_|     ║ */
 /* ║                             |_|                                                 |_|                 ║ */
 /* ║                                                                                                     ║ */
-/* ║   Last update: 05/08/2025 21:01:51                                                                  ║ */
+/* ║   Last update: 05/08/2025 21:17:19                                                                  ║ */
 /* ║   User update: Gustavo Filgueiras <gfilgueirasrj@gmail.com>                                         ║ */
 /* ║   Project:     Sou Nail Desing                                                                      ║ */
 /* ║                                                                                                     ║ */
@@ -30,7 +30,7 @@ class TesteController extends Controller
     {
         $users = User::all();
 
-        // Pode rrar cmo J
+        // Pode rrar cmo
         return response()->json($users);
     }
 
