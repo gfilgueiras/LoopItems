fd05eafa575e9f8b9791b84550491eab3ae3854b
Gustavo Filgueiras <gfilgueirasrj@gmail.com>
2025-08-12 20:33:57 -0300

    TESTES-FINAIS

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -1,4 +1,22 @@
 <?php
+/* ╔═════════════════════════════════════════════════════════════════════════════════════════════════════╗ */
+/* ║       _____                                      _____                   _                          ║ */
+/* ║      / ___ \       _                            (____ \                 | |                         ║ */
+/* ║     | |   | | ____| |_  ___  ____  _   _  ___    _   \ \ ____ _   _ ____| | ___  ____   ____  ____  ║ */
+/* ║     | |   | |/ ___)  _)/ _ \|  _ \| | | |/___)  | |   | / _  ) | | / _  ) |/ _ \|  _ \ / _  )/ ___) ║ */
+/* ║     | |___| ( (___| |_| |_| | | | | |_| |___ |  | |__/ ( (/ / \ V ( (/ /| | |_| | | | ( (/ /| |     ║ */
+/* ║      \_____/ \____)\___)___/| ||_/ \____(___/   |_____/ \____) \_/ \____)_|\___/| ||_/ \____)_|     ║ */
+/* ║                             |_|                                                 |_|                 ║ */
+/* ║                                                                                                     ║ */
+/* ║   Author:      Gustavo Filgueiras <gfilgueirasrj@gmail.com>                                         ║ */
+/* ║   Created at:  12/08/2025 20:33:57                                                                  ║ */
+/* ║                                                                                                     ║ */
+/* ║   Last update: 12/08/2025 20:33:57                                                                  ║ */
+/* ║   User update: Gustavo Filgueiras <gfilgueirasrj@gmail.com>                                         ║ */
+/* ║   Project:     Base Project Laravel (RVA)                                                           ║ */
+/* ║   License:     GNU                                                                                  ║ */
+/* ║   Copyright:   2025 Octopus Developer                                                               ║ */
+/* ╚═════════════════════════════════════════════════════════════════════════════════════════════════════╝ */
 
 declare(strict_types=1);
 
@@ -29,7 +47,7 @@ class StatusController extends Controller
 
     public function store(Request $request)
     {
-        // -- cria registro vindo do create
+        // -- cria registro vindo do creat
     }
 
     public function show(string $id)
